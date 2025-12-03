6766d1dde5b8aa1865fa740742335aa60802a255
alibuzcuk <ypretrial@gmail.com>
2025-12-03 21:38:52 +0300

    Add copyright and license information to yt-dlp-gui
    Added copyright notice and licensing information.

--- yt-dlp-gui.py
+++ yt-dlp-gui.py
@@ -1,3 +1,9 @@
+# yt-dlp-gui
+# Copyright (C) 2025 alibuzcuk
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 import tkinter as tk
 from tkinter import filedialog, messagebox, ttk
 import threading
